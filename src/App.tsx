e9489625b11d3713d96ebfcda23f047745b17e7a
Niko Huttu <niko.huttu@solita.fi>
2024-03-21 10:36:45 +0200

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState('');
 
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  }); 
+  
   return (
     <>
+    <div>{data} dataaaa</div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
